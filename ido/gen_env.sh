f805fa4b77ca2dbf2eb50ed6c2dc587be1bda9c9
HYD <hanyd@pku.edu.cn>
2021-08-23 10:03:46 +0800

    env files

---
+++ ido/gen_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo 'export SPLIT_DIR=/home/ubuntu/SwarmSense/NIHCHEST/SplitResult-patient-1-1-1'
+echo "export SLNUM=$1"
+echo "export SNNUM=$1"
+echo "export GPU=$1"
+echo 'export WEIGHTAGE=50'
---
+++ ido/sn.sh
@@ -0,0 +1,8 @@
+docker container rm sn-1 ; bash ./swarm-learning/bin/run-sn  \
+    --name=sn-1              \
+    --network sl-net \
+    --host-ip=sn-1      \
+    --sentinel-ip=sn-1  \
+    --apls-ip apls      \
+    -serverAddress spire-server \
+    -genJoinToken
